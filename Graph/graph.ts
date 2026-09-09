11bf9e5e15ac3a24ed11c1db9d8ee62bb6558f7d
Okamissu <kamil.m.kobylarz@gmail.com>
2026-09-09 08:59:08 +0200

    feat: implement addEdge() to Graph class; improve type safety

--- Graph/graph.ts
+++ Graph/graph.ts
@@ -1,11 +1,35 @@
 class Graph {
-  adjacencyList: Record<string, unknown[]>;
+  private adjacencyList: Record<string, string[]>;
 
   constructor() {
     this.adjacencyList = {};
   }
 
   addVertex(vertex: string) {
-    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+    if (!this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex] = [];
+    }
+  }
+
+  addEdge(vertex1: string, vertex2: string) {
+    this.addVertex(vertex1);
+    this.addVertex(vertex2);
+
+    this.adjacencyList[vertex1].push(vertex2);
+    this.adjacencyList[vertex2].push(vertex1);
   }
 }
+
+const graph = new Graph();
+
+graph.addVertex('A');
+graph.addVertex('B');
+graph.addVertex('C');
+graph.addVertex('D');
+
+graph.addEdge('A', 'B');
+graph.addEdge('A', 'C');
+graph.addEdge('B', 'D');
+graph.addEdge('C', 'D');
+
+console.log(JSON.stringify(graph, null, 2));
